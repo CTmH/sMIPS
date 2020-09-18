9c7f5710edb4a3609e2c71dab81fa19da2d6b77b
CTmH <eziokm@outlook.com>
2020-09-18 23:15:59 +0800

    finish SPI bus and modify SRAM

--- driver_sram.v
+++ driver_sram.v
@@ -1,23 +1,19 @@
 `timescale 1ns / 1ps
 module driver_sram(
-            input wire        sck, //clock
-            input wire        rst,
-            input wire        cs_n, //chip select
-            input wire [35:0] mosi, //slave input
-            output reg [15:0] miso, //slave output
+            input wire         sck, //clock
+            input wire         rst,
+            input wire         cs_n, //chip select
+            input wire [35:0]  mosi, //slave input
+            output wire [15:0] miso, //slave output
             );
-   reg [18:0]                 addr;
-   reg [15:0]                 wdata;
-   wire [15:0]                rdata;
+   wire [18:0]                addr;
    wire [15:0]                data;
    wire                       rw; //r=0,w=1
-   reg                        oe;
-   reg                        ce;
-   reg                        we;
-   reg                        ub;
-   reg                        lb;
-
-   assign rw = mosi[35];
+   wire                       oe;
+   wire                       ce;
+   wire                       we;
+   wire                       ub;
+   wire                       lb;
 
    sram sram0 (
              .addr(addr),
@@ -29,42 +25,15 @@ module driver_sram(
              .data(data)
              );
 
-   assign rdata = data;
-   assign data = rw ? wdata:{16{1'bz}};
-
-   always @ (posedge sck) begin
-      if(rst) begin
-         addr <= 19'b0;
-         wdata <= 16'b0;
-         miso <= 16'b0;
-         oe <= 1;
-         ce <= 1;
-         we <= 1;
-         ub <= 0;
-         lb <= 0;
-      end
-      else if(cs_n == 0) begin
-         if(rw == 0) begin
-            addr <= mosi[18:0];
-            oe <= 0;
-            ce <= 0;
-            we <= 1;
-            lb <= 0;
-            ub <= 0;
-            miso <= rdata;
-         end
-         else begin
-            wdata <= mosi[34:19];
-            addr <= mosi[18:0];
-            miso <= 16'b0;
-            oe <= 1;
-            ce <= 0;
-            we <= 0;
-            lb <= 0;
-            ub <= 0;
-         end // else: !if(rw == 0)
-      end // if (cs_n == 0)
-   end // always @ (posedge sck)
+   assign rw = mosi[35];
+   assign miso = rw ? 16'b0:data;
+   assign data = rw ? mosi[34:19]:{16{1'bz}};
+   assign addr = mosi[18:0];
+   assign oe = cs_n | rw;
+   assign ce = cs_n;
+   assign we = cs_n | (~rw);
+   assign ub = cs_n;
+   assign lb = cs_n;
 
 endmodule // driver_sram
 
